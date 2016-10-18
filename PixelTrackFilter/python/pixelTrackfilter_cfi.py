36842f8b10bfbdf0634e3a557e5795a191387528
Kong <kongPro@kongpro.dyndns.cern.ch>
2016-10-18 15:07:09 +0200

    add python

--- PixelTrackFilter/python/pixelTrackfilter_cfi.py
+++ PixelTrackFilter/python/pixelTrackfilter_cfi.py
@@ -1,11 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 pixelTrackFilter = cms.EDFilter('PixelTrackFilter',
                                   multMax = cms.double(100000),
-                                  multMin = cms.double(120),
+                                  multMin = cms.double(1),
                                   etaMax = cms.double(2.4),
                                   etaMin = cms.double(-2.4),
                                   doGenParticle = cms.bool(False),
+                                  doDS = cms.bool(False),
                                   genSrc = cms.InputTag("genParticles"),
                                   vertexSrc = cms.InputTag("offlinePrimaryVertices"),
-                                  trackSrc = cms.InputTag("generalTracks")
+                                  trackSrc = cms.InputTag("generalTracks"),
+                                  pfCandSrc = cms.InputTag("particleFlow")
                                   )
+
